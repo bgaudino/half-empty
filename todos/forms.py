c19b8ebd796342fdd96cdce8d12b40c35e9a21e7
bgaudino <bgaudino@gmail.com>
2024-01-28 08:43:04 -0600

    Collapse todo add form

--- todos/forms.py
+++ todos/forms.py
@@ -8,6 +8,7 @@ class TodoForm(forms.ModelForm):
         model = models.Todo
         fields = ('name', 'deadline', 'description')
         widgets = {
+            'name': forms.TextInput({'placeholder': 'Add something to do', 'aria-label': 'Name'}),
             'description': forms.Textarea({'rows': 3}),
             'deadline': forms.DateTimeInput({'type': 'date'}),
         }

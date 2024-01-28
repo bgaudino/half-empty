117030ca90e5dbea359dd4f4df8b385c077626f2
bgaudino <bgaudino@gmail.com>
2024-01-28 11:50:10 -0600

    Add label for new todo

--- todos/forms.py
+++ todos/forms.py
@@ -7,8 +7,11 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = models.Todo
         fields = ('name', 'deadline', 'description', 'tags')
+        labels = {
+            'name': 'New todo'
+        }
         widgets = {
-            'name': forms.TextInput({'placeholder': 'Add something to do', 'aria-label': 'Name'}),
+            'name': forms.TextInput({'autoFocus': True}),
             'description': forms.Textarea({'rows': 3}),
             'deadline': forms.DateTimeInput({'type': 'date'}),
         }

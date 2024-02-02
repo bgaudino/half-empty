9dfe9580dcf9006f8576c10d2f53dec45e22d0b5
bgaudino <bgaudino@gmail.com>
2024-02-02 10:48:02 -0600

    Add email backend

--- half_empty/settings_live.py
+++ half_empty/settings_live.py
@@ -14,3 +14,4 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 SERVER_EMAIL = DEFAULT_FROM_EMAIL = 'bgaudino@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

2f67281b90534dc9ab91e7972af1d1653b67f5c8
bgaudino <bgaudino@gmail.com>
2024-02-03 12:38:41 -0600

    Use MessageMixin in ContactFormView

--- core/views.py
+++ core/views.py
@@ -7,38 +7,6 @@ from .forms import ContactForm
 from .models import ContactFormSubmission
 
 
-class ContactFormView(CreateView):
-    model = ContactFormSubmission
-    form_class = ContactForm
-    success_url = reverse_lazy('index')
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return ContactFormSubmission.objects.filter(user=self.request.user)
-        return ContactFormSubmission.objects.filter(user__isnull=True)
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        if self.request.user.is_authenticated:
-            kwargs['user'] = self.request.user
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recaptcha_site_key'] = settings.RECAPTCHA_SITE_KEY
-        return context
-
-    def form_invalid(self, form):
-        if errors := form.errors.get('g-recaptcha-response'):
-            messages.add_message(self.request, messages.ERROR, 'reCaptcha: {}'.format('\n'.join(errors)))
-        return super().form_invalid(form)
-
-    def form_valid(self, form):
-        res = super().form_valid(form)
-        messages.add_message(self.request, messages.SUCCESS, 'Your message is sent. Someone will get back to you soon!')
-        return res
-
-
 class MessageMixin(FormView):
     success_messages = ['Form successfully submitted']
     error_messages = ['Please correct the following errors']
@@ -68,3 +36,31 @@ class MessageMixin(FormView):
         res = super().form_valid(form)
         self.add_success_messages()
         return res
+
+
+class ContactFormView(MessageMixin, CreateView):
+    model = ContactFormSubmission
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
+    success_messages = ['Your message is sent. Someone will get back to you soon!']
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return ContactFormSubmission.objects.filter(user=self.request.user)
+        return ContactFormSubmission.objects.filter(user__isnull=True)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        if self.request.user.is_authenticated:
+            kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recaptcha_site_key'] = settings.RECAPTCHA_SITE_KEY
+        return context
+
+    def form_invalid(self, form):
+        if errors := form.errors.get('g-recaptcha-response'):
+            messages.add_message(self.request, messages.ERROR, 'reCaptcha: {}'.format('\n'.join(errors)))
+        return super().form_invalid(form)
